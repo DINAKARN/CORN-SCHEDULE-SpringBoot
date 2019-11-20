9fe0bc09bce0e42059fcec1f3e90d0352d68af10
Dinakar Naramala <dnaramala@CIBER-INDIA.IN>
2019-11-20 16:15:48 +0530

    changes in controller'

--- src/main/java/com/Schedule/Corn_Schedule/Tasks/controller/Scdule_Corn.java
+++ src/main/java/com/Schedule/Corn_Schedule/Tasks/controller/Scdule_Corn.java
@@ -9,12 +9,12 @@ public class Scdule_Corn {
 
 	@Scheduled(fixedDelay = 10000)
 	public void test_ScheduleInterval() {
-		System.out.println("This is message is printing every 10 seconds");
+		System.out.println("This message is printing every 10 seconds");
 	}
 
 	// Cron sinature is <second> <minute> <hour> <day-of-month> <month> <day-of-week> <year> <command>
 	@Scheduled(cron = "0 * * ? * *")
 	public void test_ScheduleIntervalWithCron() {
-		System.out.println("This is message is printing every 1 second With cron Config");
+		System.out.println("This message is printing every 1 minute With cron Config");
 	}
 }
